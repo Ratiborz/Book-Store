a3eb6d59e19fd40d9267b171ca07d23d7500aafa
Heorhi Zaturkevich <konisgung@gmail.com>
2023-12-07 20:28:37 +0300

    Minimize version

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     plugins: [new MiniCssExtractPlugin(),
               new HtmlWebpackPlugin({
                 template: './src/index.pug',
